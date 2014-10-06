6f6a07291522de3764d0bc1b99fa9af4906822e7
Jesse <jchisu31@gmail.com>
2014-10-06 10:45:18 -0700

    Snake-Update

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,3 +1,18 @@
+var snake;
 
+var context;
+var ScreenWidth;
+var ScreenHeight;
 
+function gameInitialize() {
+    var canvas = document.getElementByID("game-screen");
+    context = canvas.getContext(2d);
+}
 
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}
