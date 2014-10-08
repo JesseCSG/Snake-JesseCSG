42453f5d77e10a54c9c95847f9552a708ec2d513
Jesse <jchisu31@gmail.com>
2014-10-08 09:21:05 -0700

    Snake-Update

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,11 +1,15 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var ScreenWidth;
 var ScreenHeight;
 
 gameInitialize();
+snakeInitialize();
 gameDraw();
+snakeDraw();
 
 
 function gameInitialize() {
@@ -26,4 +30,36 @@ function gameLoop() {
 function gameDraw() {
     context.fillStyle = "rgb(19, 63, 168)";
     context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 15;
+    snakeSize = 40;
+    
+    for(var index = snakeLength -1; index >=0; index--){
+        snake.push({
+            x: index,
+            y:0
+        });
+    }
+}
+
+function snakeDraw() {
+    for(var index = 0; index < snake.length; index++){
+        context.fillStyle = "rgb(219, 187, 42)";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
+}
+
+function snakeUpdate() {
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop();    
+    snakeTail.y = snakeHeadY;
+    snakeTail.x = snakeheadX;
+    snake.unshift(snakeTail);
 }
