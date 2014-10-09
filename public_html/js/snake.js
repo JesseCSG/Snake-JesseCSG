47b9f118ade13643c04ff2bfb88f8441e503f41e
Jesse <jchisu31@gmail.com>
2014-10-09 09:19:56 -0700

    Snake-Update

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,16 +1,30 @@
+/*-----------------------------------------------------------------------------
+ * Variiables
+ *-----------------------------------------------------------------------------
+ */
+
 var snake;
 var snakeLength;
 var snakeSize;
 
+var food;
+
 var context;
 var ScreenWidth;
 var ScreenHeight;
+/*-----------------------------------------------------------------------------
+ * Executing Game Functions
+ * ----------------------------------------------------------------------------
+ */
 
 gameInitialize();
 snakeInitialize();
-gameDraw();
-snakeDraw();
-
+foodInitialize();
+setInterval(gameLoop, 1000/3);
+/*-----------------------------------------------------------------------------
+ * Game Functions
+ * ----------------------------------------------------------------------------
+ */
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
@@ -24,18 +38,25 @@ function gameInitialize() {
 }
 
 function gameLoop() {
-    
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
+    foodDraw();
 }
 
 function gameDraw() {
-    context.fillStyle = "rgb(19, 63, 168)";
+    context.fillStyle = "blue";
     context.fillRect(0, 0, screenWidth, screenHeight);
 }
+/*-----------------------------------------------------------------------------
+ * Snake Function
+ * ----------------------------------------------------------------------------
+ */
 
 function snakeInitialize() {
     snake = [];
-    snakeLength = 15;
-    snakeSize = 40;
+    snakeLength = 5;
+    snakeSize = 30;
     
     for(var index = snakeLength -1; index >=0; index--){
         snake.push({
@@ -47,7 +68,7 @@ function snakeInitialize() {
 
 function snakeDraw() {
     for(var index = 0; index < snake.length; index++){
-        context.fillStyle = "rgb(219, 187, 42)";
+        context.fillStyle = "lime";
         context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
     }
 }
@@ -60,6 +81,22 @@ function snakeUpdate() {
     
     var snakeTail = snake.pop();    
     snakeTail.y = snakeHeadY;
-    snakeTail.x = snakeheadX;
+    snakeTail.x = snakeHeadX;
     snake.unshift(snakeTail);
+}
+/*-----------------------------------------------------------------------------
+ * Food Functions
+ * ----------------------------------------------------------------------------
+ */
+
+function foodInitialize() {
+    food = {
+        x: 0,
+        y: 0
+    };
+}
+
+function foodDraw() {
+    context.fillStyle = "gray";
+    context.fillRect(food.x, food.y, snakeSize, snakeSize);
 }
